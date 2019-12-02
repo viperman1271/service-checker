f606c6d12596f54d167e5aee63093f96a3bc1eb3
Michael Filion <viperman1271@hotmail.com>
2019-12-02 15:51:22 -0500

    First version of the service-checker with libssh2

--- main2.cpp
+++ main2.cpp
@@ -22,150 +22,144 @@
 #include <assert.h>
 
 #define SERVICE_BIND_ERROR -1
-#define SERVICE_BIND_FATAL_ERROR -100
+#define SERVICE_BIND_FATAL_ERROR -2
 #define SERVICE_BIND_RUNNING 1
-#define SERVICE_BIND_STOPPED 0
+#define SERVICE_BIND_STOPPED 2
 
-static int waitsocket(int socket_fd, LIBSSH2_SESSION *session)
+std::string read_result(LIBSSH2_CHANNEL* channel)
 {
-    struct timeval timeout;
-    int rc;
-    fd_set fd;
-    fd_set *writefd = nullptr;
-    fd_set *readfd = nullptr;
-    int dir;
+    int bytecount = 0;
+    int rc = 0;
 
-    timeout.tv_sec = 10;
-    timeout.tv_usec = 0;
+    std::stringstream ss;
 
-    FD_ZERO(&fd);
+    do
+    {
+        char buffer[0x4000];
+        rc = libssh2_channel_read(channel, buffer, sizeof(buffer));
+        if (rc > 0)
+        {
+            bytecount += rc;
+            std::cout << "we read: " << buffer << std::endl;
+            ss << buffer;
+        }
+        else if (rc != LIBSSH2_ERROR_EAGAIN)
+        {
+            std::cerr << "libssh2_channel_read returned " << rc << std::endl;
+        }
+    } while (rc > 0);
 
-    FD_SET(socket_fd, &fd);
-
-    /* now make sure we wait in the correct direction */
-    dir = libssh2_session_block_directions(session);
-
-    if (dir & LIBSSH2_SESSION_BLOCK_INBOUND)
-        readfd = &fd;
-
-    if (dir & LIBSSH2_SESSION_BLOCK_OUTBOUND)
-        writefd = &fd;
-
-    rc = select(socket_fd + 1, readfd, writefd, nullptr, &timeout);
-
-    return rc;
+    return ss.str();
 }
 
-int is_bind_running2(/*ssh_session session*/)
+LIBSSH2_CHANNEL* open_channel(LIBSSH2_SESSION* session)
 {
-//     ssh_channel channel = ssh_channel_new(session);
-//     if (channel == nullptr)
-//     {
-//         ssh_disconnect(session);
-//         ssh_free(session);
-// 
-//         return SERVICE_BIND_FATAL_ERROR;
-//     }
-// 
-//     int rc = ssh_channel_open_session(channel);
-//     if (rc != SSH_OK)
-//     {
-//         ssh_channel_free(channel);
-//         return rc;
-//     }
-// 
-//     rc = ssh_channel_request_exec(channel, "service named status");
-//     if (rc != SSH_OK)
-//     {
-//         std::cerr << "Error requesting exec: " << ssh_get_error(session) << std::endl;
-// 
-//         ssh_channel_close(channel);
-//         ssh_channel_free(channel);
-//         return rc;
-//     }
-// 
-//     std::stringstream ss;
-// 
-//     int nbytes;
-//     char buffer[1024];
-//     memset(buffer, 0, sizeof(buffer));
-// 
-//     nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-//     while (nbytes > 0)
-//     {
-//         ss << buffer;
-//         memset(buffer, 0, sizeof(buffer));
-//         nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-//     }
-// 
-//     ssh_channel_send_eof(channel);
-//     ssh_channel_close(channel);
-//     ssh_channel_free(channel);
-// 
-//     std::regex running_regex("(named).*?(is running)", std::regex_constants::ECMAScript | std::regex_constants::icase);
-//     if (std::regex_search(ss.str(), running_regex)) 
-//     {
-//         return SERVICE_BIND_RUNNING;
-//     }
-// 
-//     std::regex stopped_regex("(named).*?(is).*?(stopped)", std::regex_constants::ECMAScript | std::regex_constants::icase);
-//     if (std::regex_search(ss.str(), stopped_regex))
-//     {
-//         return SERVICE_BIND_STOPPED;
-//     }
+    LIBSSH2_CHANNEL* channel = libssh2_channel_open_session(session);
+    if (!channel)
+    {
+        std::cerr << "Unable to open a channel" << std::endl;
+        libssh2_session_disconnect(session, "");
+        libssh2_session_free(session);
+
+        return nullptr;
+    }
+
+    return channel;
+}
+
+int is_bind_running(LIBSSH2_SESSION* session)
+{
+    LIBSSH2_CHANNEL* channel = open_channel(session);
+    if (channel == nullptr)
+    {
+        return -1;
+    }
+
+    int rc = 0;
+    if (rc = libssh2_channel_exec(channel, "service named status"))
+    {
+        std::cerr << "Unable to execute on a channel" << std::endl;
+
+        libssh2_channel_close(channel);
+
+        libssh2_session_disconnect(session, "");
+        libssh2_session_free(session);
+
+        return -1;
+    }
+
+    std::string rs = read_result(channel);
+
+    if (rc = libssh2_channel_close(channel))
+    {
+        return SERVICE_BIND_ERROR;
+    }
+
+    if (rc == 0)
+    {
+        char* exitsignal = "none";
+        auto exitcode = libssh2_channel_get_exit_status(channel);
+        libssh2_channel_get_exit_signal(channel, &exitsignal, nullptr, nullptr, nullptr, nullptr, nullptr);
+    }
+
+    libssh2_channel_free(channel);
+    channel = nullptr;
+
+    std::regex running_regex("(named).*?(is running)", std::regex_constants::ECMAScript | std::regex_constants::icase);
+    if (std::regex_search(rs, running_regex)) 
+    {
+        return SERVICE_BIND_RUNNING;
+    }
+
+    std::regex stopped_regex("(named).*?(is).*?(stopped)", std::regex_constants::ECMAScript | std::regex_constants::icase);
+    if (std::regex_search(rs, stopped_regex))
+    {
+        return SERVICE_BIND_STOPPED;
+    }
 
     return SERVICE_BIND_ERROR;
 }
 
-int start_bind2(/*ssh_session session*/)
+int start_bind(LIBSSH2_SESSION* session)
 {
-//     ssh_channel channel = ssh_channel_new(session);
-//     if (channel == nullptr)
-//     {
-//         ssh_disconnect(session);
-//         ssh_free(session);
-// 
-//         return SERVICE_BIND_FATAL_ERROR;
-//     }
-// 
-//     int rc = ssh_channel_open_session(channel);
-//     if (rc != SSH_OK)
-//     {
-//         ssh_channel_free(channel);
-// 
-//         return rc;
-//     }
-// 
-//     rc = ssh_channel_request_exec(channel, "service named start");
-//     if (rc != SSH_OK)
-//     {
-//         std::cerr << "Error requesting exec: " << ssh_get_error(session) << std::endl;
-// 
-//         ssh_channel_close(channel);
-//         ssh_channel_free(channel);
-// 
-//         return rc;
-//     }
-// 
-//     std::stringstream ss;
-// 
-//     int nbytes;
-//     char buffer[1024];
-//     memset(buffer, 0, sizeof(buffer));
-// 
-//     nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-//     while (nbytes > 0)
-//     {
-//         ss << buffer;
-//         memset(buffer, 0, sizeof(buffer));
-//         nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-//     }
-// 
-//     ssh_channel_send_eof(channel);
-//     ssh_channel_close(channel);
-//     ssh_channel_free(channel);
+    LIBSSH2_CHANNEL* channel = open_channel(session);
+    if (channel == nullptr)
+    {
+        return SERVICE_BIND_ERROR;
+    }
 
-    return 0;
+    int rc = 0;
+    if (rc = libssh2_channel_exec(channel, "service named start"))
+    {
+        std::cerr << "Unable to execute on a channel" << std::endl;
+
+        libssh2_channel_close(channel);
+
+        libssh2_session_disconnect(session, "");
+        libssh2_session_free(session);
+
+        return SERVICE_BIND_ERROR;
+    }
+
+    std::string rs = read_result(channel);
+
+
+    if (rc = libssh2_channel_close(channel))
+    {
+        return SERVICE_BIND_ERROR;
+    }
+
+    if (rc == 0)
+    {
+        char* exitsignal = "none";
+        auto exitcode = libssh2_channel_get_exit_status(channel);
+        libssh2_channel_get_exit_signal(channel, &exitsignal, nullptr, nullptr, nullptr, nullptr, nullptr);
+    }
+
+    libssh2_channel_free(channel);
+    channel = nullptr;
+
+    return SERVICE_BIND_RUNNING;
 }
 
 int main(int argc, char** argv)
@@ -208,7 +202,7 @@ int main(int argc, char** argv)
     sin.sin_family = AF_INET;
     sin.sin_port = htons(22);
     sin.sin_addr.s_addr = hostaddr;
-    if (connect(sock, (struct sockaddr*)(&sin), sizeof(struct sockaddr_in)) != 0) 
+    if (connect(sock, reinterpret_cast<struct sockaddr*>(&sin), sizeof(struct sockaddr_in)) != 0) 
     {
         std::cerr << "failed to connect!" << std::endl;
         return -1;
@@ -246,92 +240,31 @@ int main(int argc, char** argv)
     privkey_path = privKeyStr;
     pubkey_path = pubKeyStr;
 #else
-    #pragma error("Not implemented")
+    privkey_path = "~/.ssh/id_rsa";
+    pubkey_path = "~/.ssh/id_rsa.pub";
 #endif // _WIN32
 
-    if (libssh2_userauth_publickey_fromfile(session, "root", pubkey_path, privkey_path, "")) 
+    if (rc = libssh2_userauth_publickey_fromfile(session, "root", pubkey_path, privkey_path, ""))
     {
-        std::cerr << "\tAuthentication by public key failed" << std::endl;
+        std::cerr << "\tAuthentication by public key failed " << rc << std::endl;
         libssh2_session_disconnect(session, "");
         libssh2_session_free(session);
 
         return -1;
     }
 
-    LIBSSH2_CHANNEL* channel = libssh2_channel_open_session(session);
-    if (!channel) 
+    rc = is_bind_running(session);
+
+    if (rc == SERVICE_BIND_STOPPED)
     {
-        std::cerr << "Unable to open a session" << std::endl;
-        libssh2_session_disconnect(session, "");
-        libssh2_session_free(session);
-
-        return -1;
-    }
-
-    while ((rc = libssh2_channel_exec(channel, "service named status")) == LIBSSH2_ERROR_EAGAIN) 
-    {
-        waitsocket(sock, session);
-    }
-
-    //////////////////////////////////////////////////////////////////////////
-
-    int bytecount = 0;
-
-    for (;;) {
-        /* loop until we block */
-        int rc;
-        do {
-            char buffer[0x4000];
-            rc = libssh2_channel_read(channel, buffer, sizeof(buffer));
-            if (rc > 0) 
-            {
-                int i;
-                bytecount += rc;
-
-                std::cout << "we read: " << buffer << std::endl;
-
-//                 fprintf(stderr, "We read:\n");
-//                 for (i = 0; i < rc; ++i)
-//                 {
-//                     fputc(buffer[i], stderr);
-//                 }
-//                 fprintf(stderr, "\n");
-            }
-            else 
-            {
-                if (rc != LIBSSH2_ERROR_EAGAIN)
-                {
-                    std::cerr << "libssh2_channel_read returned " << rc << std::endl;
-                }
-            }
-        } while (rc > 0);
-
-        /* this is due to blocking that would occur otherwise so we loop on
-           this condition */
-        if (rc == LIBSSH2_ERROR_EAGAIN) {
-            waitsocket(sock, session);
+        rc = start_bind(session);
+        if (rc != SERVICE_BIND_ERROR)
+        {
+            rc = is_bind_running(session);
         }
-        else
-            break;
     }
 
-    //////////////////////////////////////////////////////////////////////////
-
-    while ((rc = libssh2_channel_close(channel)) == LIBSSH2_ERROR_EAGAIN)
-    {
-        waitsocket(sock, session);
-    }
-
-    char* exitsignal = (char*)"none";
-
-    if (rc == 0) 
-    {
-        auto exitcode = libssh2_channel_get_exit_status(channel);
-        libssh2_channel_get_exit_signal(channel, &exitsignal, nullptr, nullptr, nullptr, nullptr, nullptr);
-    }
-
-    libssh2_channel_free(channel);
-    channel = nullptr;
+    int result = (rc == SERVICE_BIND_RUNNING) ? 0 : -1;
 
     libssh2_session_disconnect(session, "normal shutdown");
     libssh2_session_free(session);
@@ -344,5 +277,5 @@ int main(int argc, char** argv)
 
     libssh2_exit();
 
-    return 0;
+    return result;
 }
